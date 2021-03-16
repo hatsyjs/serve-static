a9684a6320dc743d7b449794ce8ba9b1d7c1fbb4
Ruslan Lopatin <ruslan.lopatin@gmail.com>
2021-03-16 18:45:01 +0700

    Generate declaration map

--- rollup.config.js
+++ rollup.config.js
@@ -39,6 +39,9 @@ export default {
         flatDts({
           tsconfig: 'tsconfig.main.json',
           lib: true,
+          compilerOptions: {
+            declarationMap: true,
+          },
         }),
       ],
     },
